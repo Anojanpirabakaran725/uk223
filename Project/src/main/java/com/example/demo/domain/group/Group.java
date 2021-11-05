d5bbdb6558000c45d1385a695bdfa3336dc5333e
unknown <enes.spahiu@noseryoung.ch>
2021-11-05 17:09:41 +0100

    Commit: Minor changes

---
+++ Project/src/main/java/com/example/demo/domain/group/Group.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain.group;
+
+import com.example.demo.domain.appUser.User;
+
+import java.util.List;
+
+public class Group {
+
+    private List<User> users;
+    private String name;
+    private String desc;
+
+
+
+}
